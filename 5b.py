bbacf9ebd3875950433ca864e887f383f162f003
Prakyath1234 <prakyath.23cs097@sode-edu.in>
2024-10-03 15:31:46 +0530

    Add files via upload

---
+++ 5b.py
@@ -0,0 +1,14 @@
+import  matplotlib.pyplot as plt
+exp_vals=[1400,600,300,410,250]
+exp_labels=["Home Rent","Food","Phone/InternetBill","Call","OtherUtilities"]
+plt.pie(exp_vals,labels=exp_labels,shadow=True)
+plt.show()
+
+plt.pie(exp_vals,labels=exp_labels,shadow=True,autopct='%0.1f%%',radius=1.5)
+plt.show()
+
+plt.pie(exp_vals,labels=exp_labels,shadow=True,autopct='%0.1f%%',radius=1.5,explode=[0,0,0,0.2,0.1])
+plt.show()
+
+plt.pie(exp_vals,labels=exp_labels,shadow=True,autopct='%0.1f%%',radius=1.5,explode=[0,0,0,0.1,0.2],counterclock=False, startangle=30)
+plt.show()
